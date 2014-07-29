7c9a0a5d6f8f65c2a81b02f13acf8bdaff7ab6c1
fangjie <hbxffj@163.com>
2014-07-29 21:39:28 +0800

    在类中方法传递相同的实例对象，不能直接使用类构造的属性，除非使用val,var

--- src/com/fangj/Test2.scala
+++ src/com/fangj/Test2.scala
@@ -17,9 +17,32 @@ package com.fangj
  * @date: 下午1:34:49
  * @version: V1.0
  */
+
+
+
 object Test2 {
+class Rational2(val n: Int, val d: Int) {
+
+  require(d != 0)
+
+  override def toString = n + "/" + d
+
+  def add(a: Rational2): Rational2 =
+
+    new Rational2(n * a.d + a.n * d, d * a.d)
+
+}
   implicit def aa(n: Int) = new { def next = n + 1 }
   def main(args: Array[String]) {
+
+    val oneHalf = new Rational2(1, 2)
+
+    val twoThirds = new Rational2(2, 3)
+
+    val s = oneHalf add twoThirds
+
+    println(s)
+
     val list = new java.util.ArrayList[Int]() with ForEachAble[Int] with JsonAble
     list.add(1); list.add(2)
     list.foreach(println(_))
