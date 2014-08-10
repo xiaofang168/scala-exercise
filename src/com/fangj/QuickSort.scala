65f1dceccee2d5a563af3fc5ad86d588b1f1e24d
fangjie <hbxffj@163.com>
2014-08-10 12:10:14 +0800

    增加了插入排序

--- src/com/fangj/QuickSort.scala
+++ src/com/fangj/QuickSort.scala
@@ -6,6 +6,8 @@
  */
 package com.fangj
 
+import scala.annotation.tailrec
+
 /**
  * 快速算法的原理
  * @ClassName: QuickSort
@@ -27,4 +29,25 @@ object QuickSort extends App {
       quickSort(x1) ::: head :: quickSort(x2)
     }
   }
+
+  @tailrec
+  def _insertSort[T](x1: List[T], x2: List[T])(p: (T, T) => Boolean): List[T] = x2 match {
+    case Nil => x1
+    case x2_head :: x2_tail =>
+      val (x1_left, x1_right) = x1.partition(x => p(x, x2_head))
+      val x1_new = (x1_left ::: (x2_head :: x1_right))
+      _insertSort(x1_new, x2_tail)(p)
+  }
+
+  def insertSort[T](xs: List[T])(p: (T, T) => Boolean) = xs match {
+    case Nil => Nil
+    case head :: tail => _insertSort(head :: Nil, tail)(p)
+  }
+
+  // 定义比较函数
+  val compare = (x1: Int, x2: Int) => x1 < x2;
+  //生成测试样本
+  val intList = List(3, 4, 2, 1, 8, 7, 6, 3)
+  insertSort(intList)(compare)
 }
+
