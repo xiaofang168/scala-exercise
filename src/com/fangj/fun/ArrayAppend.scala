92ddd9178a57594ffd1a8be275fa0b8faa37a1c0
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-08-11 09:03:03 +0800

    array append

---
+++ src/com/fangj/fun/ArrayAppend.scala
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2014 The Hikvision CO.Ltd
+ * site: http://www.hikvision.com
+ * Prject: scala-exercise
+ * Description: ArrayAppend.scala
+ * created at: 2014年8月8日
+ */
+package com.fangj.fun
+
+/**
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @Date: 2014年8月8日 下午1:05:07
+ * @version: $Rev$
+ */
+object ArrayAppend {
+  def main(args: Array[String]) {
+	  val a = Array(1,2)
+	  val b = Array(3,4)
+	  val c = a++b
+	  println(c mkString (""))
+  }
+}
