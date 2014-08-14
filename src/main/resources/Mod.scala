dc588f3bea398bcd24ee3182bf61fe69e8c06dad
fangjie <hbxffj@163.com>
2014-08-15 00:22:01 +0800

    增加了添加依赖的用法import，非简单的

--- src/main/resources/Mod.scala
+++ src/main/resources/Mod.scala
@@ -1,4 +1,12 @@
-trait Mod extends (String => Boolean) {
-  def apply(a: String):Boolean = if(a=="ddd") true else false
+trait Mod extends (String => String) {
+  import java.text.SimpleDateFormat
+  import java.util.Date
+  import java.sql.Connection
+  def apply(a: String): String = {
+    val c = List(2,3,5,5)
+    c.foreach(println(_))
+    val d2 = new Date()
+    new SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(d2)
+  }
 }
-new Mod { }
+new Mod {}
