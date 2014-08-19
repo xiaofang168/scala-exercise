aa24cd765b3c504e1a232de9cdbf34f4614a86ac
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-08-19 10:15:48 +0800

    更改了读取文件路径的方法

--- src/main/scala/com/fangj/twitter/Test.scala
+++ src/main/scala/com/fangj/twitter/Test.scala
@@ -19,11 +19,7 @@ import scala.io.Codec
  */
 object Test {
   def main(args: Array[String]) {
-    val file = new File("/home/fangjie/git/scala-exercise/src/main/resources/Mod.scala")
-    val file1 = new File("/home/fangjie/git/scala-exercise/src/main/resources/Derived.scala")
-    //Console println Eval("1+1")
-    val sum = Eval[Int]("1 + 1")
-    println(sum)
+    val file = new File(getClass().getResource("/Mod.scala").getPath())
     def text = Source.fromFile( file )(Codec.UTF8).mkString
     println(text)
     val derived = (new Eval).apply[(String) => String](s"$text \n new Mod {}")
