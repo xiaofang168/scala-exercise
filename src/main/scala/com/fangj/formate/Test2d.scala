ee0e186db229302fd1266345d7f6bd646e4c329a
fangjie <hbxffj@163.com>
2014-09-06 11:08:03 +0800

    scala 格式化数字类型

---
+++ src/main/scala/com/fangj/formate/Test2d.scala
@@ -0,0 +1,21 @@
+/*
+ * @FileName: Test2d.scala
+ * @Prject:scala-exercise
+ * @Package: com.fangj.formate
+ * @date: 2014-9-6
+ */
+package com.fangj.formate
+
+/**
+ * @ClassName: Test2d
+ * @Description: TODO
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @time: 上午11:05:22
+ * @version: V1.0
+ */
+object Test2d extends App {
+  val a = 166
+  val b = "%04d".format(a)
+  Console println b
+
+}
