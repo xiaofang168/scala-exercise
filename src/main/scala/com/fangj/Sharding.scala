9c98f50ca3145d7e5dec97d5239e5250ef0462d2
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2014-09-30 15:59:13 +0800

    多个类型隐式转换使用一个方法

---
+++ src/main/scala/com/fangj/Sharding.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2014 The Hikvision CO.Ltd
+ * site: http://www.hikvision.com
+ * Prject: dbproxy-services
+ * Description: Sharding.scala
+ * created at: 2014年9月23日
+ */
+package com.hikvision.dbproxy.services.support
+
+/**
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @Date: 2014年9月23日 下午6:03:33
+ * @version: $Rev$
+ */
+object Sharding {
+  import java.math.BigDecimal
+  import java.math.BigInteger
+
+	
+  implicit def toStr[T](x: T)(implicit ev: (Int with Float with Double with Long) <:< T) = x.toString
+  implicit def toDouble[T](x:T)(implicit ev: (BigInteger with BigDecimal) <:< T) = x.toString.toDouble
+    
+  def apply(id: Double): Boolean = {
+    id>300 && id<400
+  }
+				    
+  
+}
