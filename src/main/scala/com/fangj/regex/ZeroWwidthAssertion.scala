b700a4adc82dc542f61cfb51e5729144cfa10ca5
jeff <hbxffj@163.com>
2014-11-28 15:24:22 +0800

    增加了零宽断言表达式例子

---
+++ src/main/scala/com/fangj/regex/ZeroWwidthAssertion.scala
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2014 The Hikvision CO.Ltd
+ * site: http://www.hikvision.com
+ * Prject: scala-exercise
+ * Description: ZeroWwidthAssertion.scala
+ * created at: 2014年11月28日
+ */
+package com.fangj.regex
+
+/**
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @Date: 2014年11月28日 下午3:06:50
+ * @version: $Rev$
+ */
+object ZeroWwidthAssertion extends App {
+  val regex = """(.*)(?<=/)""".r
+  val result = regex.findFirstIn("E:/workspaces/scala-workspace/spark-start/dd") match {
+    case Some(regex(s1)) => (s1)
+    case _ => (null, null, null)
+  }
+  println(result)
+}
