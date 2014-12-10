f4e4d45142711dbc31df18c9a1738dd4141f9b98
jeff <hbxffj@163.com>
2014-12-10 19:11:32 +0800

    增加了下划线分割，首字母大写

---
+++ src/main/scala/com/fangj/regex/FirstCapitalize.scala
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2014 The Hikvision CO.Ltd
+ * site: http://www.hikvision.com
+ * Prject: scala-exercise
+ * Description: FirstCapitalize.scala
+ * created at: 2014年12月10日
+ */
+package com.fangj.regex
+
+/**
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @Date: 2014年12月10日 下午7:10:24
+ * @version: $Rev$
+ */
+object FirstCapitalize extends App {
+  val a = "huang_hong_qiao".split("_").map(_.capitalize).mkString("")
+  println(a)
+}
