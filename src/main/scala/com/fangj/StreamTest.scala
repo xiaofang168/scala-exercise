7f7fbc075041a943d119087e580fee4cba41e2cb
jeff <hbxffj@163.com>
2015-01-21 22:20:22 +0800

    Streams are like list but they tailored evaluated only on demand

---
+++ src/main/scala/com/fangj/StreamTest.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2015 The Jeff CO.Ltd
+ * Prject: scala_test
+ * Description: StreamTest.scala
+ * created at: 下午10:06:29
+ */
+package com.fangj
+
+/**
+ * Streams are like list
+ * but they tailored evaluated only on demand
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @Date: 2015年1月21日
+ * @version: $Rev$
+ */
+object StreamTest {
+
+  def streamRange(lo: Int, hi: Int): Stream[Int] = {
+    println(lo + " ")
+    if (lo >= hi) Stream.Empty
+    else Stream.cons(lo, streamRange(lo + 1, hi))
+  }
+
+  def main(args: Array[String]) {
+    streamRange(1, 10).take(3).toList
+  }
+
+}
