1ea67140ac184673f372b42ca12159c1dba9620d
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2015-02-06 16:48:38 +0800

    开启流程表单的form继承基类FlowForm

--- src/main/scala/com/fangj/flowservice/Form.scala
+++ src/main/scala/com/fangj/flowservice/Form.scala
@@ -7,14 +7,15 @@
  */
 package com.fangj.flowservice
 
+import com.fangj.workflow.FlowForm
+
 /**
  * @author: <a href="mailto:hbxffj@163.com">方杰</a>
  * @Date: 2015年2月2日 上午11:41:15
  * @version: $Rev$
  */
-class Form {
+class Form extends FlowForm{
   var id: Int = _
   var name: String = _
   var creator: String = _
-
 }
