8d952c1c0eb177482d0dfe8ee44cd523c422eac8
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2015-02-06 16:47:22 +0800

    增加了开启流程表单的基类（用于获取流程编号）

---
+++ src/main/scala/com/fangj/workflow/FlowForm.scala
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2015 The Hikvision CO.Ltd
+ * site: http://www.hikvision.com
+ * Prject: scala-exercise
+ * Description: FlowForm.scala
+ * created at: 2015年2月6日
+ */
+package com.fangj.workflow
+
+/**
+ * 流程表单
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @Date: 2015年2月6日 下午4:34:27
+ * @version: $Rev$
+ */
+class FlowForm extends Serializable {
+
+  /**
+   * 流程编号
+   */
+  var flowNum: String = _
+
+}
