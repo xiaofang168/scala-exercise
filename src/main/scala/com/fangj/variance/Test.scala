abe14deec04f2c63d3beba5c8b75de1248a911c2
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2015-03-18 17:50:19 +0800

    协变、逆变

---
+++ src/main/scala/com/fangj/variance/Test.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2015 The Hikvision CO.Ltd
+ * site: http://www.hikvision.com
+ * Prject: scala-exercise
+ * Description: Test.scala
+ * created at: 2015年3月18日
+ */
+package com.fangj.variance
+
+/**
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @Date: 2015年3月18日 下午5:45:52
+ * @version: $Rev$
+ */
+trait A[+T]
+
+class C[+T] extends A[T]  // C是invariant的
+
+class X
+
+class Y extends X
+
+object Test extends App{
+  
+  val t:C[X] = new C[Y]
+  
+  val list:List[X] = List[Y]()
+  
+}
