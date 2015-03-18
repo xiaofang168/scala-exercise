01f0b21e2e037f91f4fd696cc0029671e1fe9220
fangjie1 <fangjie1@PC-FANGJIE1.hikvision.com>
2015-03-18 17:27:30 +0800

    链式调用的方法demo

---
+++ src/main/scala/com/fangj/linkcall/LinkCall.scala
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2015 The Hikvision CO.Ltd
+ * site: http://www.hikvision.com
+ * Prject: scala-exercise
+ * Description: Father.scala
+ * created at: 2015年3月18日
+ */
+package com.fangj.linkcall
+import scala.tools.reflect.ToolBox
+/**
+ * @author: <a href="mailto:hbxffj@163.com">方杰</a>
+ * @Date: 2015年3月18日 下午4:36:34
+ * @version: $Rev$
+ */
+//class A {def method1: A = this }
+class A { def method1: this.type = this } 
+class B extends A { def method2 : this.type = this } 
+
+object LinkCall extends App{
+  val b = new B
+  b.method1.method2  // ok
+}
