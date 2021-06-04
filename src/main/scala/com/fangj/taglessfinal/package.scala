514f608c255747a0542351ae463da3e8655cd15d
fangjie <fangjie@etouch.cn>
2021-06-04 14:50:34 +0800

    taglessfinal文档备注

---
+++ src/main/scala/com/fangj/taglessfinal/package.scala
@@ -0,0 +1,7 @@
+package com.fangj
+
+package object taglessfinal {
+
+  val url = "https://github.com/softwaremill/free-tagless-compare/blob/master/src/main/scala/com/softwaremill/Refactoring.scala"
+
+}
