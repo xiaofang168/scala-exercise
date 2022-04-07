675d101e113c9434243bff3995dea2623bb34481
FangJie <jiefffang@didiglobal.com>
2022-04-07 20:29:11 +0800

    支持excel文件读取

---
+++ src/main/scala/com/fangj/excel/DataBean.java
@@ -0,0 +1,98 @@
+package com.fangj.excel;
+
+/**
+ * @author jefffang
+ * @date Created in 10:45 上午 2022/4/7.
+ */
+public class DataBean {
+    private String team;
+    private String biz;
+    private String project;
+    private String indexName;
+    private String indexType;
+    private String indexDesc;
+    private String dailyTimer;
+    private String username;
+    private String url;
+    private String problem;
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getBiz() {
+        return biz;
+    }
+
+    public void setBiz(String biz) {
+        this.biz = biz;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public String getIndexName() {
+        return indexName;
+    }
+
+    public void setIndexName(String indexName) {
+        this.indexName = indexName;
+    }
+
+    public String getIndexType() {
+        return indexType;
+    }
+
+    public void setIndexType(String indexType) {
+        this.indexType = indexType;
+    }
+
+    public String getIndexDesc() {
+        return indexDesc;
+    }
+
+    public void setIndexDesc(String indexDesc) {
+        this.indexDesc = indexDesc;
+    }
+
+    public String getDailyTimer() {
+        return dailyTimer;
+    }
+
+    public void setDailyTimer(String dailyTimer) {
+        this.dailyTimer = dailyTimer;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    public String getProblem() {
+        return problem;
+    }
+
+    public void setProblem(String problem) {
+        this.problem = problem;
+    }
+}
