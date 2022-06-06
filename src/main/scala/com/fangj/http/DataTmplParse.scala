8b74341561eae79e80b37dbca0b46a7543725f72
FangJie <jiefffang@didiglobal.com>
2022-06-06 19:59:45 +0800

    使用cookies自动提交数据（替换在界面手动操作，python和perl也试过但有问题）

---
+++ src/main/scala/com/fangj/http/DataTmplParse.scala
@@ -0,0 +1,25 @@
+package com.fangj.http
+
+import com.google.common.collect.Maps
+import com.hubspot.jinjava.Jinjava
+
+import java.util
+import scala.io.Source
+
+object DataTmplParse {
+
+  def main(args: Array[String]): Unit = {
+    val str: String = parse("_2001__12_28", "09:50:00")
+    println(str)
+  }
+
+  def parse(source: String, time: String): String = {
+    val jinjava = new Jinjava()
+    val context: util.Map[String, String] = Maps.newHashMap
+    context.put("source", source)
+    context.put("time", time)
+    val template = Source.fromFile(this.getClass.getResource("data_tmpl.json").getPath).mkString
+    jinjava.render(template, context)
+  }
+
+}
