1654f09fade26691771047211b10880849ee1280
FangJie <jiefffang@didiglobal.com>
2022-06-06 19:59:45 +0800

    使用cookies自动提交数据（替换在界面手动操作，python和perl也试过但有问题）

--- src/main/scala/com/fangj/http/PostWithCookies.scala
+++ src/main/scala/com/fangj/http/PostWithCookies.scala
@@ -1,27 +1,19 @@
 package com.fangj.http
 
-import com.alibaba.fastjson.JSON
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.entity.StringEntity
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.util.EntityUtils
+import com.alibaba.fastjson.{JSON, JSONArray, JSONObject}
 
 import scala.io.Source
 
-object PostWithCookies extends App {
-  val url = "http:///report/v2/monitor/config/save"
-  val client = HttpClients.createDefault
-  val httpPost = new HttpPost(url)
-  httpPost.addHeader("Content-Type", "application/json;charset=utf-8")
-  httpPost.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36")
-  // cookie必传
-  httpPost.addHeader("Cookie", "=d110571c686cc1f80489bc4ab37a0abc000280000;username=fangjie")
-  httpPost.addHeader("Host", "")
-  val json = Source.fromFile(this.getClass.getResource("data.json").getPath).mkString
-  val jsonStr = JSON.parseObject(json).toString
-  httpPost.setEntity(new StringEntity(jsonStr, "utf-8"))
-  val resp = client.execute(httpPost)
-  val respEntity = resp.getEntity
-  val str: String = EntityUtils.toString(respEntity, "UTF-8")
-  print(str)
+object PostWithCookies {
+
+  def main(args: Array[String]): Unit = {
+    val json = Source.fromFile(this.getClass.getResource("/batch_data.json").getPath).mkString
+    val array: JSONArray = JSON.parseArray(json)
+    array.forEach(e => {
+      val body: String = e.asInstanceOf[JSONObject].toJSONString
+      val result = DomainHttpClientUtil.postJson("http:///report/v2/monitor/config/save", body, "233dda557f5e4904bbdcd2ddc564c765000280000")
+      println(result)
+    })
+  }
+
 }
