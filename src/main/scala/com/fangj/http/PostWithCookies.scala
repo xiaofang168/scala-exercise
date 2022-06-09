877485b0c1490e527970a606d168d20100c486fe
FangJie <jiefffang@didiglobal.com>
2022-06-09 18:06:47 +0800

    使用circe替换fastjson解析

--- src/main/scala/com/fangj/http/PostWithCookies.scala
+++ src/main/scala/com/fangj/http/PostWithCookies.scala
@@ -1,6 +1,8 @@
 package com.fangj.http
 
-import com.alibaba.fastjson.{JSON, JSONArray, JSONObject}
+import com.alibaba.fastjson.{JSON, JSONArray}
+import io.circe.Json
+import io.circe.parser._
 
 import scala.io.Source
 
@@ -8,12 +10,25 @@ object PostWithCookies {
 
   def main(args: Array[String]): Unit = {
     val json = Source.fromFile(this.getClass.getResource("/batch_data.json").getPath).mkString
-    val array: JSONArray = JSON.parseArray(json)
-    array.forEach(e => {
-      val body: String = e.asInstanceOf[JSONObject].toJSONString
-      val result = DomainHttpClientUtil.postJson("http://bigdata.xiaojukeji.com/report/v2/monitor/config/save", body, "233dda557f5e4904bbdcd2ddc564c765000280000")
+    // circe用法
+    val parseResult = parse(json).getOrElse(Json.Null)
+    val size: Int = parseResult.hcursor.values.get.size
+    println(s"数组大小：$size")
+    parseResult.hcursor.values.get.foreach(e => {
+      val body = e.hcursor.top.get.noSpaces
+      val result = DomainHttpClientUtil.postJson("http://bigdata.xiaojukeji.com/report/v2/monitor/config/save", body, "c6e4eb950c5840991d13b69db788ecd2000280000")
       println(result)
+      //println(e.hcursor.downField("dispatchTime").as[String].getOrElse(">>>"))
     })
+    // fastjson用法
+    val array: JSONArray = JSON.parseArray(json)
+    println("数组大小：" + array.size())
+    //    val array: JSONArray = JSON.parseArray(json)
+    //    array.forEach(e => {
+    //      val body: String = e.asInstanceOf[JSONObject].toJSONString
+    //      val result = DomainHttpClientUtil.postJson("http://bigdata.xiaojukeji.com/report/v2/monitor/config/save", body, "c6e4eb950c5840991d13b69db788ecd2000280000")
+    //      println(result)
+    //    })
   }
 
 }
