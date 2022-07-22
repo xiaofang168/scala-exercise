0f502f3c6491026d17c4b9204a5372837e9f0f19
FangJie <jiefffang@didiglobal.com>
2022-07-22 11:09:07 +0800

    zio shiftOnFuture

---
+++ src/main/scala/com/fangj/zio/ShiftOnFuture.scala
@@ -0,0 +1,28 @@
+package com.fangj.zio
+
+import cats.effect.ExitCode
+
+import java.util.concurrent.Executors
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+object ShiftOnFuture extends App with PrintThread {
+
+  implicit val ec =
+    ExecutionContext.fromExecutor(Executors.newFixedThreadPool(2))
+
+  def loop(id: String)(i: Int): Future[Unit] =
+    for {
+      _ <- Future(printThread(id))
+      _ <- Future(Thread.sleep(200))
+      result <- loop(id)(i + 1)
+    } yield result
+
+  val program = for {
+    _ <- loop("A")(0)
+    _ <- loop("B")(0)
+  } yield ExitCode.Success
+
+  Await.result(program, Duration.Inf)
+}
+
