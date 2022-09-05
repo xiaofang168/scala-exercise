6c09fc5121720c289650ace0c73665ccbd10c77c
fangjie <fangjie@etouch.cn>
2022-09-05 18:33:10 +0800

    文件处理

--- src/main/scala/com/fangj/http/DomainHttpClientUtil.scala
+++ src/main/scala/com/fangj/http/DomainHttpClientUtil.scala
@@ -7,14 +7,14 @@ import org.apache.http.util.EntityUtils
 
 object DomainHttpClientUtil {
 
-  def postJson(url: String, body: String, : String): String = {
+  def postJson(url: String, body: String, ddcTicket: String): String = {
     val client = HttpClients.createDefault
     val httpPost = new HttpPost(url)
     httpPost.addHeader("Content-Type", "application/json;charset=utf-8")
     httpPost.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36")
     // cookie必传
-    httpPost.addHeader("Cookie", s"=$;username=jefffang")
-    httpPost.addHeader("Host", "")
+    httpPost.addHeader("Cookie", s"ddc_ticket=$ddcTicket;username=jefffang")
+    httpPost.addHeader("Host", "..com")
     httpPost.setEntity(new StringEntity(body, "utf-8"))
     val resp = client.execute(httpPost)
     val respEntity = resp.getEntity
